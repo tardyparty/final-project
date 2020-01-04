b2c03039e7eed4cfe5e4359d04397f0f823f191a
tardyparty <tardyparty@users.noreply.github.com>
2020-01-04 11:29:16 -0600

    controller setup

--- finalProject/controllers/inventoryController.js
+++ finalProject/controllers/inventoryController.js
@@ -3,31 +3,31 @@ const db = require("../models");
 // Defining methods for the booksController
 module.exports = {
   findAll: function(req, res) {
-    db.Book
+    db.Inventory
       .find(req.query)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   findById: function(req, res) {
-    db.Book
+    db.Inventory
       .findById(req.params.id)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   create: function(req, res) {
-    db.Book
+    db.Inventory
       .create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   update: function(req, res) {
-    db.Book
+    db.Inventory
       .findOneAndUpdate({ _id: req.params.id }, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   remove: function(req, res) {
-    db.Book
+    db.Inventory
       .findById({ _id: req.params.id })
       .then(dbModel => dbModel.remove())
       .then(dbModel => res.json(dbModel))
--- finalProject/routes/api/books.js
+++ finalProject/routes/api/books.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const booksController = require("../../controllers/booksController");
+const booksController = require("../../controllers/inventoryController");
 
 // Matches with "/api/books"
 router.route("/")
