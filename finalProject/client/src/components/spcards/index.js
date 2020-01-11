d5095184b025f270c78e4200f3175c7bf0e3e05a
SoInZane <51385126+SoInZane@users.noreply.github.com>
2020-01-11 09:58:17 -0600

    added fonts, styled and corrected syntax

--- finalProject/client/src/components/spcards/index.js
+++ finalProject/client/src/components/spcards/index.js
@@ -4,76 +4,76 @@ import './style.css';
 class SPCards extends React.Component {
     render() {
         return (
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="row">
-                            <div class="col-md-3 text-center">
-                                <div class="card border-light">
-                                    <h5 class="card-title">
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-md-12">
+                        <div className="row">
+                            <div className="col-md-3 text-center">
+                                <div className="card border-light">
+                                    <h5 className="card-title">
                                         Basic Car Camper
                                                 </h5>
-                                    <img class="card-img-top" alt="Bootstrap Thumbnail First" src="/images/SP_basic.jpeg" />
-                                    <div class="card-block">
+                                    <img className="card-img-top" alt="Bootstrap Thumbnail First" src="/images/SP_basic.jpeg" />
+                                    <div className="card-block">
 
 
-                                        <p class="card-text">
+                                        <p className="card-text">
                                             For the Overnight Car Camper. This package includes the following:
 							                        </p>
                                         <p>
-                                            <a class="btn btn-primary" href="#">Rent Yours Today!</a>
+                                            <a className="btn btn-primary" href="#">Rent Yours Today!</a>
                                         </p>
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-md-3 text-center">
-                                <div class="card border-light">
-                                    <h5 class="card-title">
+                            <div className="col-md-3 text-center">
+                                <div className="card border-light">
+                                    <h5 className="card-title">
                                         Glam/Party Camper
                                                 </h5>
-                                    <img class="card-img-top" alt="Bootstrap Thumbnail First" src="/images/SP_glam.jpeg" />
-                                    <div class="card-block">
+                                    <img className="card-img-top" alt="Bootstrap Thumbnail First" src="/images/SP_glam.jpeg" />
+                                    <div className="card-block">
 
 
-                                        <p class="card-text">
+                                        <p className="card-text">
                                             For the Camper who enjoys comfort even more than they love the outdoors. This package included the follwoing:
 							                        </p>
                                         <p>
-                                            <a class="btn btn-primary" href="#">Rent Yours Today!</a>
+                                            <a className="btn btn-primary" href="#">Rent Yours Today!</a>
                                         </p>
                                     </div>
                                 </div>
                             </div>
                             <div class="col-md-3 text-center">
-                                <div class="card border-light">
-                                    <h5 class="card-title">
+                                <div className="card border-light">
+                                    <h5 className="card-title">
                                         Couple Campers
 							                </h5>
-                                    <img class="card-img-top" alt="Bootstrap Thumbnail Second" src="/images/SP_couple.jpeg" />
-                                    <div class="card-block">
+                                    <img className="card-img-top" alt="Bootstrap Thumbnail Second" src="/images/SP_couple.jpeg" />
+                                    <div className="card-block">
 
-                                        <p class="card-text">
+                                        <p className="card-text">
                                             For the Camper couple or friends who just want to get away together. This package includes the following:
 							                        </p>
                                         <p>
-                                            <a class="btn btn-primary" href="#">Rent Yours Today!</a>
+                                            <a className="btn btn-primary" href="#">Rent Yours Today!</a>
                                         </p>
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-md-3 text-center">
-                                <div class="card border-light">
-                                    <h5 class="card-title">
+                            <div className="col-md-3 text-center">
+                                <div className="card border-light">
+                                    <h5 className="card-title">
                                         Solo Camper
 							                </h5>
-                                    <img class="card-img-top" alt="Bootstrap Thumbnail Third" src="/images/SP_solo.jpeg" />
-                                    <div class="card-block">
+                                    <img className="card-img-top" alt="Bootstrap Thumbnail Third" src="/images/SP_solo.jpeg" />
+                                    <div className="card-block">
 
-                                        <p class="card-text">
+                                        <p className="card-text">
                                             For the Camper who is at one with nature. This package includes the following:
 							                        </p>
                                         <p>
-                                            <a class="btn btn-primary" href="#">Rent Yours Today!</a>
+                                            <a className="btn btn-primary" href="#">Rent Yours Today!</a>
                                         </p>
                                     </div>
                                 </div>
