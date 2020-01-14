d6354586339bb8708125e3c3bd8666a4e131e97c
SoInZane <51385126+SoInZane@users.noreply.github.com>
2020-01-14 09:40:02 -0600

    added headers for images on community, styled, added tablet media query

--- finalProject/client/src/components/social/index.js
+++ finalProject/client/src/components/social/index.js
@@ -6,70 +6,92 @@ class Social extends React.Component {
     render() {
         return (
 
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="row">
-                            <div class="col-md-2">
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-md-12">
+                        <div className="row">
+                            <div className="col-md-2">
                                 <img alt="Bootstrap Image Preview" src="/images/community_children.jpeg" />
+                                <h4>Best Daddy Ever</h4>
                             </div>
-                            <div class="col-md-8">
-                                <p>
-                                    Do not and I repeat, do not tell your kids ghost stories while camping. Our kids refused to sleep in the tent with everyone else and would only slept in car for the rest of the trip.
+                            <div className="col-md-1">
+
+                            </div>
+                            <div className="col-md-7">
+                                <p id="community-text">
+                                    Do not and I repeat, do not tell your kids ghost stories while camping. Our kids refused to sleep in the tent with everyone else and would only sleep in the car for the rest of the trip.
                                 </p>
                             </div>
-                            <div class="col-md-2">
-                                <button type="button" class="btn btn-success">
-                                    Button
-					</button>
+                            <div className="col-md-2">
+                                <button type="button" className="btn btn-success">
+                                    Share Your Experience
+					            </button>
 
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="row">
-                            <div class="col-md-2">
+                <div className="row">
+                    <div className="col-md-12">
+                        <div className="row">
+                            <div className="col-md-2">
                                 <img alt="Bootstrap Image Preview" src="/images/community_connect_nature.jpeg" />
+                                <h4>Reconnected Camper</h4>
                             </div>
-                            <div class="col-md-8">
-                                <p>
-                                    There is something about disconnecting from everything and reconnecting with nature. My obligations keep me way too busy but when I do get out here it feels like home
+                            <div className="col-md-1">
+
+                            </div>
+                            <div className="col-md-7">
+                                <p id="community-text">
+                                    There is something about disconnecting from everything and reconnecting with nature. My obligations keep me way too busy but when I do get out here it feels like home.
                                 </p>
+                            </div>
+                            <div className="col-md-2">
+
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="row">
-                            <div class="col-md-2">
+                <div className="row">
+                    <div className="col-md-12">
+                        <div className="row">
+                            <div className="col-md-2">
                                 <img alt="Bootstrap Image Preview" src="/images/community_toilet_paper.jpeg" />
+                                <h4>Toilet Paper Bandit</h4>
                             </div>
-                            <div class="col-md-8">
-                                <p>
+                            <div className="col-md-1">
+
+                            </div>
+                            <div className="col-md-7">
+                                <p id="community-text">
                                     Imagine an entire group of “EXPERIENCED” campers and no one remembered to walk with toilet paper……………………. Let’s just say everyone got really INVENTIVE for our three-day trip lol lol lol
                                 </p>
                             </div>
+                            <div className="col-md-2">
+
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="row">
-                            <div class="col-md-2">
+                <div className="row">
+                    <div className="col-md-12">
+                        <div className="row">
+                            <div className="col-md-2">
                                 <img alt="Bootstrap Image Preview" src="/images/community_wine_friends.jpeg" />
+                                <h4>Wine Girls</h4>
                             </div>
-                            <div class="col-md-8">
-                                <p>
-                                    We may not be your average camper but with this glamping set up we will go again and again, glamping + good friends + wine = a good time
+                            <div className="col-md-1">
+
+                            </div>
+                            <div className="col-md-7">
+                                <p id="community-text">
+                                    We may not be your average camper but with this glamping set up we will go again and again, glamping + good friends + wine = a good time!
                                 </p>
                             </div>
-                            <div class="col-md-2">
-                                <button type="button" class="btn btn-success">
-                                    Button
-					</button>
+                            <div className="col-md-2">
+                                <button type="button" className="btn btn-success">
+                                    Share Your Experience
+					            </button>
 
                             </div>
                         </div>
