d4e4b15e14f9df2adf326c62956b9e5c20e123f5
tardyparty <tardyparty@users.noreply.github.com>
2020-01-14 19:01:34 -0600

    bootstrap works now

--- finalProject/client/src/index.js
+++ finalProject/client/src/index.js
@@ -3,8 +3,8 @@ import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import React from 'react';
 import store from "./store";
+import 'bootstrap/dist/css/bootstrap.min.css'; 
 
-// import App from "./components/App";
 
 ReactDOM.render((
   <Provider store={store}>
