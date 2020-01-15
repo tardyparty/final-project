1e2e64aa2ca81948dcc8c7e9baa2591e5307bc34
Rebecca Bartl <rebecca.bartl@gmail.com>
2020-01-15 10:09:36 -0600

    camp api

--- finalProject/client/src/components/searchArea/index.js
+++ finalProject/client/src/components/searchArea/index.js
@@ -8,6 +8,22 @@ const searchArea = (props) => {
                 <button type="submit">Search</button>
             </form>
         </div>
+        // <form>
+        //     <div className="form-group">
+        //         <label className="search-area"><h3> Search by State</h3></label>
+        //         <br></br>
+        //         <input className="col-12 form-control"
+        //             value={props.searchField}
+        //             type="text"
+        //             name="searchCampsite"
+        //             placeholder="Enter State Abbrev"
+        //             onChange={props.handleSearch}
+        //         />
+        //     </div>
+        //     <button type="submit" className="submitBtn btn btn-primary" onClick={props.handleFormSubmit}>
+        //         Submit
+        //     </button>
+        // </form>
     )
 }
 
