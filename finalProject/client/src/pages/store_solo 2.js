d3a26ed6af04b56b467e2d77325e8931b0a74e22
tardyparty <tardyparty@users.noreply.github.com>
2020-01-14 19:14:15 -0600

    fixed merge issues

---
+++ finalProject/client/src/pages/store_solo 2.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import Header from "../components/Nav";
+import Footer from "../components/footer";
+import Spsolo from "../components/spsolo"
+import { Container, Form, Button } from "react-bootstrap";
+
+class Store_Solo extends React.Component {
+
+    render() {
+        return (
+            <Container>
+                <Header />
+                <Container>
+                    <Container>
+                        <Spsolo>
+                            
+                        </Spsolo>
+                        
+                    </Container>
+                    <Footer>
+                    </Footer>
+                </Container>
+            </Container>
+        )
+    }
+}
+
+export default Store_Solo;
