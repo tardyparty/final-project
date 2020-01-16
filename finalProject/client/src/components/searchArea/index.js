7a253c99df4640f6336de724ca1f28351af32d4f
SoInZane <51385126+SoInZane@users.noreply.github.com>
2020-01-15 20:45:40 -0600

    added image to camp search page, styled, added css files for camp search components

--- finalProject/client/src/components/searchArea/index.js
+++ finalProject/client/src/components/searchArea/index.js
@@ -1,24 +1,36 @@
 import React from "react";
+import './style.css';
 
 const SearchArea = (props) => {
-    return(
-        <form>
-            <div className="form-group">
-                <label className="search-area"><h3> Search by State</h3></label>
-                <br></br>
-                <input className="col-12 form-control"
-                    value={props.searchField}
-                    type="text"
-                    name="searchCampsite"
-                    placeholder="Enter State Abbrev"
-                    onChange={props.handleSearch}
-                />
+    return (
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-md-12">
+                <img className="camp_search-image" src="/images/CG_search.jpeg"></img>
+                </div>
             </div>
-            <button type="submit" className="submitBtn btn btn-primary" onClick={props.handleFormSubmit}>
-                Submit
-            </button>
-        </form>
-    )
+            <div className="row">
+                <div className="col-md-12"></div>
+                
+                <form>
+                    <div className="form-group">
+                        <label className="search-area"><h3> Search by State</h3></label>
+                        <br></br>
+                        <input className="col-12 form-control"
+                            value={props.searchField}
+                            type="text"
+                            name="searchCampsite"
+                            placeholder="Enter State Abbrev"
+                            onChange={props.handleSearch}
+                        />
+                    </div>
+                    <button type="submit" className="submitBtn btn btn-primary" onClick={props.handleFormSubmit}>
+                        Submit
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
 }
 
 export default SearchArea;
